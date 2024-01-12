cc25b67fc2f37a40034f50e9946b29e9d6e7eff0
Romain Breton <romain.breton.pro@gmail.com>
2024-01-12 22:58:28 +0100

    add react router dom

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function Error() {
   return (
     <div className="t-error">
-      <h1>Home</h1>
+      <h1>Error</h1>
       <Link to={`/`}>Home</Link>
     </div>
   )
--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function Signin() {
   return (
     <div className="t-error">
-      <h1>Home</h1>
+      <h1>Sign in</h1>
       <Link to={`/`}>Home</Link>
     </div>
   )
